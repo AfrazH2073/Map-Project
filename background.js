e5dd62ed0dbf112a0d0e1255d884cde838c003a6
Uliyaah Dionisio <uliyaahd@gmail.com>
2024-04-06 22:20:39 -0700

    Added console logs

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) =>{
     if (tab.url && tab.url.includes("www.google.com/maps/")&& tab.url.includes("@")){
+        if(tab.url.includes("place")){
+            
+        }
         const queryParameters = tab.url.split("@")[1];
         console.log(queryParameters);
         const urlParameters = new URLSearchParams(queryParameters);
-
         console.log(urlParameters);
+
         chrome.tabs.sendMessage(tabId, {
+            type: "Unlabeled",
             locationId: urlParameters.get("")
         });
     }
--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,19 @@
 (() =>{
     let googleReport, googleMap;
+    let currentLocation = "";
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const {value, locationId} = obj;
 
-        
+        currentLocation = locationId;
+        newReportAdded();
     });
+
+    const newReportAdded = () => {
+        const reportBtn = document.createElement(img);
+
+        reportBtn.src = chrome.runtime.getURL("../assets/reportIcon.png");
+        reportBtn.className = "report-btn";
+        reportBtn.title = "Click to report a road block";
+    }
 })();
